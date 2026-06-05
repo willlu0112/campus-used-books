7b6552fca8db44ff1d2534fa8a4af9a24cb3c64a
willllu0112-arch <willllu0112@gmail.com>
2026-06-05 16:39:12 +0800

    Add Spring Boot project files

---
+++ campus-used-books/src/main/java/com/example/campus_used_books/CampusUsedBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_used_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusUsedBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusUsedBooksApplication.class, args);
+	}
+
+}
---
+++ campus-used-books/src/test/java/com/example/campus_used_books/CampusUsedBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_used_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusUsedBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
