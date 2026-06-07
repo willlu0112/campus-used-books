cfbbdec2bc391612715f34257f5c7a8c2ddb17e4
seb <237876753+a1135534-seb@users.noreply.github.com>
2026-06-07 18:36:13 +0800

    Feat: complete basic CRUD operation
    -builded basic create,read,update,delete bridge talking between front end database.

--- src/main/java/com/example/campus_used_books/controller/HomeController.java
+++ src/main/java/com/example/campus_used_books/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -25,4 +26,24 @@ public class HomeController {
         bookRepository.save(book);
         return "redirect:/";
     }
+
+    @PostMapping("/deleteBook")
+    public String deleteBook(@RequestParam Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editBook")
+    public String editBook(@RequestParam Long id, Model model) {
+        Book book = bookRepository.findById(id).get(); 
+        model.addAttribute("book", book); 
+        return "edit"; 
+    }
+
+    @PostMapping("/updateBook")
+    public String updateBook(Book book) {
+        bookRepository.save(book); 
+        return "redirect:/";
+    }
+
 }
