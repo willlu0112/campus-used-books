ce21872d0afd5113ab174819710b0d77954c1347
seb <237876753+a1135534-seb@users.noreply.github.com>
2026-06-07 17:27:17 +0800

    Feat: Basic communication front-end-database

--- src/main/java/com/example/campus_used_books/controller/HomeController.java
+++ src/main/java/com/example/campus_used_books/controller/HomeController.java
@@ -1,13 +1,28 @@
 package com.example.campus_used_books.controller;
 
+import com.example.campus_used_books.entity.Book;
+import com.example.campus_used_books.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private BookRepository bookRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) { 
+        model.addAttribute("books", bookRepository.findAll()); 
         return "index"; 
+    }
+
+    @PostMapping("/addBook")
+    public String addBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/";
     }
 }
